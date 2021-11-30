467166d9923b185756d996f241ba4d9415b639e2
wolfram-180 <sergei.ulvis@gmail.com>
2021-11-30 15:52:17 +0300

    added funcs

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 import 'transaction.dart';
 
@@ -19,16 +20,20 @@ class MyHomePage extends StatelessWidget {
     Transaction(
       id: '1',
       title: 'new shoes',
-      amount: 3500.50,
+      amount: 35.51,
       date: DateTime.now(),
     ),
     Transaction(
       id: '2',
       title: 'products',
-      amount: 2894.56,
+      amount: 28.56,
       date: DateTime.now(),
     ),
   ];
+  //late String titleInput;
+  //late String amountInput;
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -37,7 +42,7 @@ class MyHomePage extends StatelessWidget {
         title: Text('Flutter App'),
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        //mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Container(
@@ -49,8 +54,80 @@ class MyHomePage extends StatelessWidget {
             ),
           ),
           Card(
-            color: Colors.red,
-            child: Text('TX'),
+            elevation: 5,
+            child: Container(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: <Widget>[
+                  TextField(
+                    decoration: InputDecoration(labelText: 'Title'),
+                    controller: titleController,
+                    //      onChanged: (value) {
+                    //        titleInput = value;
+                    //     },
+                  ),
+                  TextField(
+                    decoration: InputDecoration(labelText: 'Amount'),
+                    controller: amountController,
+                    // onChanged: (value) => amountInput = value,
+                  ),
+                  FlatButton(
+                      onPressed: () {
+                        print(titleController.text);
+                        //    print(titleInput);
+                        //    print(amountInput);
+                      },
+                      child: Text('Add Transaction'),
+                      textColor: Colors.purple),
+                ],
+              ),
+            ),
+          ),
+          Column(
+            children: transactions.map((tx) {
+              return Card(
+                child: Row(children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.black,
+                        width: 2,
+                      ),
+                    ),
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      '\$ ${tx.amount}',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.purple,
+                      ),
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        tx.title,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                      Text(
+                        DateFormat.yMMMd().format(tx.date),
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
+                  )
+                ]),
+              );
+            }).toList(),
           ),
         ],
       ),
