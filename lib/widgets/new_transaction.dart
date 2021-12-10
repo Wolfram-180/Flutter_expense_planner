ceac207ffe85465b657c2e27ed4aae58c3203faf
wolfram-180 <sergei.ulvis@gmail.com>
2021-12-10 14:18:35 +0300

    additional refactoring

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -6,16 +6,44 @@ import 'adaptive_flat_button.dart';
 
 class NewTransaction extends StatefulWidget {
   late final Function addTx;
-  @override
-  State<NewTransaction> createState() => _NewTransactionState();
 
-  NewTransaction(this.addTx);
+  NewTransaction(this.addTx) {
+    print('Constructor NewTransaction Widget');
+  }
+
+  @override
+  State<NewTransaction> createState() {
+    print('createState NewTransaction Widget');
+    return _NewTransactionState();
+  }
 }
 
 class _NewTransactionState extends State<NewTransaction> {
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
   DateTime _selectedDate = DateTime.now();
+
+  _NewTransactionState() {
+    print('Constructor NewTransaction State');
+  }
+
+  @override
+  void initState() {
+    print('initState()');
+    super.initState();
+  }
+
+  @override
+  void didUpdateWidget(covariant NewTransaction oldWidget) {
+    print('didUpdateWidget()');
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  void dispose() {
+    print('dispose()');
+    super.dispose();
+  }
 
   void _submitData() {
     if (_amountController.text.isEmpty || _titleController.text.isEmpty) {
